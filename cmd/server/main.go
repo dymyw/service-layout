9feec46c3331cc39f82b0d3b9c20c14e9f7aeeeb
dymyw <dymayongwei@163.com>
2021-01-12 17:49:12 +0800

    feature: signal exit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
@@ -15,19 +19,40 @@ import (
 const port = ":50001"
 
 func main() {
+	log.Println("server start")
+
 	listen, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
-	s := grpc.NewServer()
-	hello.RegisterGreeterServer(s, &service.GreeterService{})
-	account.RegisterAccountServer(s, &service.AccountService{})
+	server := grpc.NewServer()
+	hello.RegisterGreeterServer(server, &service.GreeterService{})
+	account.RegisterAccountServer(server, &service.AccountService{})
+
+	exitSignals := []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT} // SIGTERM is POSIX specific
+	c := make(chan os.Signal, len(exitSignals))
+	signal.Notify(c, exitSignals...)
+	for {
+		s := <-c
+		log.Printf("get a signal %s", s.String())
+
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			server.Stop()
+			log.Println("server stop")
+			time.Sleep(time.Second)
+			return
+		case syscall.SIGHUP:
+		default:
+			return
+		}
+	}
 
 	// for test
-	reflection.Register(s)
+	reflection.Register(server)
 
-	if err := s.Serve(listen); err != nil {
+	if err := server.Serve(listen); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
